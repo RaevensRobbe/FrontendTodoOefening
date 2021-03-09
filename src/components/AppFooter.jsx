d30643f7bc8347e6486e0242c4e82a01a696d421
Robbe <robbe.raevens@student.howest.be>
2021-03-09 10:35:30 +0100

    Push alles tot begin les

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppFooter = () => {
+    return (
+        <div className="o-row">
+            <footer className="o-container">
+            <p>&copy; R.R - Powered by 🦊</p>
+            </footer>
+        </div>
+    );
+}
+
+export default AppFooter;
